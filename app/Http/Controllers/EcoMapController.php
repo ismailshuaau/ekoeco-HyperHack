03ecae82b4013517b27304fb3aa62e16c91faad4
ismail shuaau <ismailshuaau@gmail.com>
2019-11-09 20:23:04 +0800

    routes, controllers and model added to display the map

---
+++ app/Http/Controllers/EcoMapController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EcoMapController extends Controller
+{
+    //
+    public function show()
+    {
+        return "test";
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>ekoeco</strong>
+            <strong>This is a test</strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -65,6 +65,13 @@
                     <label for="destination">Destination</label>
                     <input type="search" id="destination" placeholder="Where are we going?" />
                 </div>
+                <div class="form-group">
+                    <input type="hidden" id="travelMode" value="DRIVING" />
+                </div>
+                {{-- <div class="form-group">
+                  <label for="budget">Budget</label>
+                  <input type="search" id="destination" placeholder="Where are we going?" />
+                </div> --}}
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
@@ -72,7 +79,6 @@
 
       <div class="album py-5 bg-light">
         <div class="container">
-
           <div class="row">
             <div class="col-md-4">
               <div class="card mb-4 box-shadow">
